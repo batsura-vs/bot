11c872a4e2b033762a12022baccf378ce8d4ff1c
voven4ek <voven4ek@gmail.com>
2024-05-29 12:37:41 +0300

    keyboard

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from aiogram.fsm.storage.memory import MemoryStorage
 import asyncio
 
 from config import TOKEN
+from keyboard import keyboard
+from user_states import UserStates
 
 storage = MemoryStorage()
 bot = Bot(token=TOKEN)
@@ -13,7 +15,8 @@ dp = Dispatcher(storage=storage)
 
 @dp.message(CommandStart())
 async def start(message: types.Message, state: FSMContext):
-    await message.answer(f"Привет! я умею следить за Пари")
+    await message.answer(f"Привет! я умею следить за Пари", reply_markup=keyboard[UserStates.BASE])
+    await state.set_state(UserStates.BASE)
 
 
 async def main():
